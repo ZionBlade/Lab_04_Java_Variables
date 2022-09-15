1583e3c5df69f5bf3644df1694902d8ef90bae57
ZionBlade <wernerct@mail.uc.edu>
2022-09-15 13:49:48 -0400

    UNfinished Project

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,50 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int intOperandA = 45;
+        int intOperandB = 13;
+
+        int intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        int intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        int intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        int intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        int intModulo = intOperandA % intOperandB;
+        System.out.println("The remainder of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+        double doubleOperandA = 4.50;
+        double doubleOperandB = 2.45;
+
+        double doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+        int familyKids = 2;
+        boolean isRaining = false;
+        double gasPrice = 3.49;
+        int favNum = 99;
+        double shoeSize = 7.5;
+        int birthMonth = 12;
+        String fullName = "Callie Werner";
+
+
+
     }
 }
